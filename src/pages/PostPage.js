527eb9e01c3db792c3d257c4c9d085a7ea927088
nima <nimaarefi@outlook.com>
2019-09-03 22:22:16 +0430

    add more fetch options to post page

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -6,12 +6,36 @@ class Post extends React.Component {
     data: null,
   }
 
-  componentDidMount() {
+  mounted = true
+
+  setStateIfMounted = (newState) => {
+    if (this.mounted) {
+      this.setState(newState)
+    }
+  }
+
+  fetchData = () => {
     const { page } = this.props.match.params
     fetch(`https://jsonplaceholder.ir/posts/${page}`)
       .then((response) => response.json())
       .then((data) => this.setState({ data }))
       .finally(() => this.setState({ loading: false }))
+  }
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  componentDidUpdate(prevProps) {
+    const { page } = this.props.match.params
+    if (prevProps.match.params.page !== page) {
+      this.setState({ loading: true })
+      this.fetchData()
+    }
+  }
+
+  componentWillUnmount() {
+    this.mounted = false
   }
 
   render() {
