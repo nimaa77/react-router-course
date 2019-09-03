e92b2909e70c4675deca812f65b3c15d7c68716a
nima <nimaarefi@outlook.com>
2019-09-03 22:40:39 +0430

    404 component better text

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,13 @@
 import React from "react"
 
 function NotFound(props) {
-  return <p>صفحه‌ایی که به دنبال آن بودید پیدا نشد!</p>
+  return (
+    <p className="text-center w-100 pt-5 mt-5">
+      404
+      <br />
+      صفحه‌ایی که به دنبال آن بودید پیدا نشد!
+    </p>
+  )
 }
 
 export default NotFound
