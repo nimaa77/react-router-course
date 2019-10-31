d059477e8ca30d80206d2ddf4033592a9f8cc5b0
nima <nimaarefi@outlook.com>
2019-10-31 21:44:45 +0330

    save state

--- src/components/AuthProvider.js
+++ src/components/AuthProvider.js
@@ -3,8 +3,19 @@ import React from "react"
 export const AuthProviderContext = React.createContext()
 export const AuthProviderContextDispatcher = React.createContext()
 
+const LOCAL_STORAGE_KEY = "authState"
+
+const initialState = () =>
+  JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || false)
+
 function AuthProvider({ children }) {
-  const [state, setState] = React.useState(false)
+  const [state, setState] = React.useState(initialState)
+
+  React.useEffect(() => {
+    const data = JSON.stringify(state)
+    localStorage.setItem(LOCAL_STORAGE_KEY, data)
+  }, [state])
+
   return (
     <AuthProviderContext.Provider value={state}>
       <AuthProviderContextDispatcher.Provider value={setState}>
