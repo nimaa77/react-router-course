df757c919dd8214e442d100ad4ab841c76aad6df
nima <nimaarefi@outlook.com>
2019-10-31 20:37:31 +0330

    use provider

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from "react"
 import { Route, Switch } from "react-router-dom"
 import routes from "./routes"
 import Template from "components/Template"
+import AuthProvider from "components/AuthProvider"
 
 function App() {
   return (
-    <Template>
-      <Switch>
-        {routes.map((route) => (
-          <Route {...route} />
-        ))}
-      </Switch>
-    </Template>
+    <AuthProvider>
+      <Template>
+        <Switch>
+          {routes.map((route) => (
+            <Route {...route} />
+          ))}
+        </Switch>
+      </Template>
+    </AuthProvider>
   )
 }
 
